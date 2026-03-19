481a0bc407debc3ab29c3e36435501b4a7286863
Mariiagrm <mariagarciaminarro2004@gmail.com>
2026-03-19 03:01:16 +0100

    Creacion de paginas principales

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector('.linkedin').addEventListener('click', function(e) {
+        // Ejemplo de acción personalizada antes de ir a la web
+        console.log("Redirigiendo a LinkedIn...");
+        window.open('https://www.linkedin.com/in/TU_USUARIO', '_blank');
+    });
